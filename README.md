1cb1ecf6719b72514e7020f5cd55ea846a1dc5aa
Qadeer~QMD <qadeerkhosa839@gmail.com>
2026-02-08 13:24:16 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+ ## Qadeer-TechX Pairing Site
+ * QR code pair
+ * 8 digit pair codes
+
+
+* [Heroku](  https://heroku.com/deploy?template=https://github.com/BOSS-MD/BOSS-Pairing-Site)
+ 
+ all regards to
+ > Qadeer-TechX Official
+>
+>
+> 
+ > BOST WORLD 🌎 BEST =Tech
---
+++ qr.js
@@ -0,0 +1,117 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+	default: Mbuvi_Tech,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	makeInMemoryStore,
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function MBUVI_MD_QR_CODE() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let Qr_Code_By_Mbuvi_Tech = Mbuvi_Tech({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			Qr_Code_By_Mbuvi_Tech.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Mbuvi_Tech.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					await delay(5000);
+					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+					await delay(800);
+				   let b64data = Buffer.from(data).toString('base64');
+				   let session = await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id, { text: 'BOSZ-AI~' + b64data });
+	
+				   let MBUVI_MD_TEXT = `
+╔════════════════════◇
+║『 SESSION CONNECTED』
+║ ✨Qadeer-TechX🔷
+║ ✨Qadeer-TechX OFFICIAL🔷
+╚════════════════════╝
+
+
+---
+
+╔════════════════════◇
+║『 YOU'VE CHOSEN Qadeer-TechX 』
+║ -Set the session ID in Heroku:
+║ - SESSION_ID: 
+╚════════════════════╝
+╔════════════════════◇
+║ 『••• _V𝗶𝘀𝗶𝘁 𝗙𝗼𝗿_H𝗲𝗹𝗽 •••』
+║❍ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: https://youtube.com/@abdulrehmanfnaax?si=ZmHFXKMOIe9eQxNz
+║❍ 𝐎𝐰𝐧𝐞𝐫: 923369423531
+║❍ 𝐑𝐞𝐩𝐨: https://github.com/jimni89y-afk/Boss_bot
+║❍ 𝐖𝐚𝐆𝗿𝐨𝐮𝐩: https://chat.whatsapp.com/Dn4ae6j5L4QLgxvP9pph2a?mode=gi_t
+║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029VbCT0WnFsn0cPZKRDu3V
+║❍ 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: _https://www.instagram.com/Mrboss
+║ ☬ ☬ ☬ ☬
+╚═════════════════════╝
+𒂀 Enjoy Qadeer-TechX
+
+
+---
+
+Don't Forget To Give Star⭐ To My Repo
+______________________________`;
+	 await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id,{text:MBUVI_MD_TEXT},{quoted:session})
+
+
+
+					await delay(100);
+					await Qr_Code_By_Mbuvi_Tech.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					MBUVI_MD_QR_CODE();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service is Currently Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await MBUVI_MD_QR_CODE()
+});
+module.exports = router
